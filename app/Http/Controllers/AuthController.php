9eb758fc4001c0fab5225107060ba7d5e6191ae6
Nurlan <nurik.droid@gmail.com>
2020-04-21 18:59:09 +0600

    user login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        // dd($user, $request->password);
+        if($user) {
+            if(Hash::check($request->password, $user->password)) {
+                $client = DB::table('oauth_clients')->find(2);
+
+                request()->request->add([
+                    'client_id' => $client->id,
+                    'client_secret' => $client->secret,
+                    'grant_type' => 'password',
+                    'username' => $request->email,
+                    'password' => $request->password,
+                ]);
+
+                $token = Request::create(
+                    '/oauth/token',
+                    'POST'
+                );
+                return Route::dispatch($token);
+
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => trans('admin.password_mismatch'),
+                ])->setStatusCode(422);
+            }
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => trans('admin.user_not_found'),
+            ])->setStatusCode(422);
+        }
+
+
+
+    }
+
+    // public function logout(Request $request)
+    // {
+    //     $value = $request->bearerToken();
+    //     $id = (new Parser())->parse($value)->getHeader('jti');
+    //     $token = $request->user()->tokens->find($id);
+    //     $token->revoke();
+    //
+    //     return response()->json([
+    //         'success' => true,
+    //         'message' => trans('admin.logged_out'),
+    //     ])->setStatusCode(200);
+    // }
+}
