7369d177d5cb88df036eedb3b854ba1e78e2f5f2
Nurlan <nurik.droid@gmail.com>
2020-05-08 11:32:42 +0600

    show if only exists

--- resources/views/mail/feedback.blade.php
+++ resources/views/mail/feedback.blade.php
@@ -4,9 +4,17 @@
 |      |    Данные      |
 | ------------- |:-------------:|
 | {{ trans('admin.name') }}      | {{ $feedback->name }}      |
+@if($feedback->email)
 | {{ trans('admin.email') }}      | {{ $feedback->email }} |
+@else
+
+@endif
 | {{ trans('admin.phone_number') }}      | {{ $feedback->phone_number }} |
+@if($feedback->comments)
 | {{ trans('admin.comments') }}      | {{ $feedback->comments }} |
+@else
+
+@endif
 @endcomponent
 
 <a href="tel::{{ $feedback->phone_number }}">Позвонить</a>
